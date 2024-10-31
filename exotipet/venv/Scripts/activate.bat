f25eee2709c31caf25ed9109e3f353140518b88b
BraDuD <bephillipduprel@gmail.com>
2024-10-30 19:33:11 -0500

    adding animals

--- exotipet/venv/Scripts/activate.bat
+++ exotipet/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Usuario\Documents\i+d+i_exotipet\exotipet\venv
+set VIRTUAL_ENV=C:\Users\User\Documents\exotipet\i-d-i_exotipet\exotipet\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
